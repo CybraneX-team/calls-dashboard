9f6c4e6935b5e55a8ce5ac349bb082bedd4658a8
Tarush Mohindru <tarushmohindru1515@gmail.com>
2025-04-14 12:25:30 +0530

    configure https url

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function DashboardPage() {
   const [phoneNumber, setPhoneNumber] = useState("")
 
   const handleCall = async (form: any) => {
-    const res = await fetch(`http://localhost:5000/outbound`, {
+    const res = await fetch(`https://urmi.ai/outbound`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
